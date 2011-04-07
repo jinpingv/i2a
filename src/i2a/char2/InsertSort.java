bc6d51b0791f93ef9b5cbd9552019661a755cf0f
Jin Ping <jinpingv@hotmail.com>
2011-04-07 06:47:27 -0700

    InsertSort

---
+++ src/i2a/char2/InsertSort.java
@@ -0,0 +1,11 @@
+package i2a.char2;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 11-4-7
+ * Time: 上午6:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class InsertSort {
+}
